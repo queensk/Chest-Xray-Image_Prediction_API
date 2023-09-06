4a68d41225eae05d3b520cf85e15cf8f87f459b0
queens kisivuli <queenskisivuli@gmail.com>
2023-09-06 08:53:40 +0300

    init files

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Chest_Xray_Image_Prediction_API;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
